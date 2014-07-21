9531bc6f00e0eda57a01b231a846e39443c303d8
Minke Zhang <minke.zhang@gmail.com>
2014-07-21 16:43:05 -0500

    added more tests, attempting to fix deadlock in File::get_pos and File::seek

--- test/concurrent.cc
+++ test/concurrent.cc
@@ -22,11 +22,20 @@ void aux_write_overwrite_test_worker(std::shared_ptr<giga::File> file, std::shar
 	std::shared_ptr<std::string> buffer (new std::string("bla"));
 
 	res += (c->get_pos() == 0);
-	// res += (c->write(buffer, false) == 3);
-	// res += (c->read(buffer, 10) == 2);
-	// res += (buffer->compare("d\n") == 0);
+	res += (c->write(buffer, false) == 3);
+	res += (c->read(buffer, 10) == 2);
+	res += (buffer->compare("d\n") == 0);
 
-	int expected = 2;
+	c->seek(-10);
+	std::cout << "seek" << std::endl;
+	res += (c->get_pos() == 0);
+	std::cout << "get_pos" << std::endl;
+	res += (c->read(buffer, 10) == 5);
+	res += (buffer->compare("blad\n") == 0);
+
+	file->close(c);
+
+	int expected = 7;
 	*result += (int) (res == expected);
 }
 
@@ -64,46 +73,14 @@ void aux_read_test_worker(std::shared_ptr<giga::File> file, std::shared_ptr<std:
 	*result += (int) (res == expected);
 }
 
-TEST_CASE("concurrent|write-overwrite") {
-/*
-	int n_threads = 16;
-	int n_attempts = 1000;
-
-	std::shared_ptr<std::atomic<int>> result (new std::atomic<int>());
-
-	for(int attempt = 0; attempt < n_attempts; attempt++) {
-		std::shared_ptr<std::string> buffer (new std::string);
-		std::vector<std::thread> threads;
-		std::shared_ptr<giga::File> file (new giga::File("test/files/five.txt", "rw", std::shared_ptr<giga::Config> (new giga::Config(2, 2, 1))));
-
-		for(int i = 0; i < n_threads; i++) {
-			threads.push_back(std::thread (aux_write_overwrite_test_worker, file, result));
-		}
-
-		while(file->get_n_clients()) {
-			// cf. http://bit.ly/1pLvXct
-			std::this_thread::sleep_for(std::chrono::microseconds(1));
-		}
-
-
-		for(int i = 0; i < n_threads; i++) {
-			threads.at(i).join();
-		}
-
-		if(((attempt + 1) % 100) == 0) {
-			std::cout << "write-overwrite attempt " << attempt + 1 << "/" << n_attempts << std::endl;
-		}
-	}
-
-	REQUIRE(*result == n_attempts * n_threads);
-*/
-}
-
 TEST_CASE("concurrent|read") {
 	int n_threads = 16;
 	int n_attempts = 1000;
 
 	std::shared_ptr<std::atomic<int>> result (new std::atomic<int>());
+
+	std::cout << "R:";
+	std::cout.flush();
 
 	for(int attempt = 0; attempt < n_attempts; attempt++) {
 		std::shared_ptr<std::string> buffer (new std::string);
@@ -125,9 +102,52 @@ TEST_CASE("concurrent|read") {
 		}
 
 		if(((attempt + 1) % 100) == 0) {
-			std::cout << "read attempt " << attempt + 1 << "/" << n_attempts << std::endl;
+			std::cout << ".";
+			std::cout.flush();
 		}
 	}
 
+	std::cout << std::endl;
+
 	REQUIRE(*result == n_attempts * n_threads);
 }
+
+TEST_CASE("concurrent|write-overwrite") {
+	int n_threads = 16;
+	int n_attempts = 1000;
+
+	std::shared_ptr<std::atomic<int>> result (new std::atomic<int>());
+
+	std::cout << "W:";
+	std::cout.flush();
+
+	for(int attempt = 0; attempt < n_attempts; attempt++) {
+		std::shared_ptr<std::string> buffer (new std::string);
+		std::vector<std::thread> threads;
+		std::shared_ptr<giga::File> file (new giga::File("test/files/five.txt", "rw", std::shared_ptr<giga::Config> (new giga::Config(2, 2, 1))));
+
+		for(int i = 0; i < n_threads; i++) {
+			threads.push_back(std::thread (aux_write_overwrite_test_worker, file, result));
+		}
+
+		while(file->get_n_clients()) {
+			// cf. http://bit.ly/1pLvXct
+			std::this_thread::sleep_for(std::chrono::microseconds(1));
+		}
+
+
+		for(int i = 0; i < n_threads; i++) {
+			threads.at(i).join();
+		}
+
+		if(((attempt + 1) % 100) == 0) {
+			std::cout << ".";
+			std::cout.flush();
+		}
+	}
+
+	std::cout << std::endl;
+
+	REQUIRE(*result == n_attempts * n_threads);
+}
+
