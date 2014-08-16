c403e3b33e5639d342272b448b447a28696ba046
Minke Zhang <minke.zhang@gmail.com>
2014-08-16 16:10:18 -0500

    added cachepp, removed old stuff

--- include/src/crypto.h
+++
@@ -1,9 +0,0 @@
-/**
- * the crypto internal library MAY be called by the user, but the author questions why
- */
-namespace giga {
-	class Crypto {
-		public:
-			static std::string hash(std::string s);
-	};
-}
--- src/crypto.cc
+++
@@ -1,6 +0,0 @@
-#include "libs/md5/md5.h"
-
-#include "src/crypto.h"
-
-std::string giga::Crypto::hash(std::string s) { return(md5(s)); }
-
