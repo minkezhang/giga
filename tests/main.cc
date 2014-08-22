6bba2f30a3280e7e31c5ed5d6d768f97fad6228a
Minke Zhang <minke.zhang@gmail.com>
2014-08-22 15:24:07 -0500

    cleaned up tests

--- tests/main.cc
+++ tests/main.cc
@@ -1,8 +1,11 @@
 #define CATCH_CONFIG_RUNNER
 
+#include <cstdlib>
+
 #include "libs/catch/catch.hpp"
 
 int main(int argc, char **argv) {
+	int i = system("make prep");
 	int result = Catch::Session().run(argc, argv);
 	return(result);
 }
